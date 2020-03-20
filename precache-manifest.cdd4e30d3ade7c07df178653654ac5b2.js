2dd13f5bd84d5cb9c392645fface3b7556cb159e
yanivk1984 <37773443+yanivk1984@users.noreply.github.com>
2020-03-20 23:39:34 +0200

    first commit

---
+++ precache-manifest.cdd4e30d3ade7c07df178653654ac5b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c74dc9d5b8e4f3fa25e6377e635fce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bfd7fa5dfb67126c4b2b",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f4fa576ca026cce95946",
+    "url": "./static/css/main.075ce9fb.chunk.css"
+  },
+  {
+    "revision": "bfd7fa5dfb67126c4b2b",
+    "url": "./static/js/2.39317165.chunk.js"
+  },
+  {
+    "revision": "55a86acb647d4d97be32c276a4a0a778",
+    "url": "./static/js/2.39317165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4fa576ca026cce95946",
+    "url": "./static/js/main.bbee3d45.chunk.js"
+  },
+  {
+    "revision": "b08d6a8eae261312f2fe",
+    "url": "./static/js/runtime-main.7c5cb8b5.js"
+  }
+]);
